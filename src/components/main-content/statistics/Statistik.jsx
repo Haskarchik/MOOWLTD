4f00a999cb8d5baaa3ce04d1cbf3af305cd8bf11
Haskarchik <vovakryt24@gmail.com>
2023-02-04 05:39:15 +0200

    adapt statistik

--- src/components/main-content/statistics/Statistik.jsx
+++ src/components/main-content/statistics/Statistik.jsx
@@ -1,10 +1,79 @@
 import React, { useEffect, useRef, useState } from "react";
 import Map from "./Map";
+import { agronomicStatistics } from "../../../data/agronomicStatistics";
 
 import "../mainContent.css";
 
 export default function Statistik(props) {
-  
+  function displayTopFiveCropCapacity() {
+    let list = [];
+
+    let array = agronomicStatistics.sort(function (a, b) {
+      if (a.cropCapacity > b.cropCapacity) {
+        return -1;
+      }
+      if (a.cropCapacity < b.cropCapacity) {
+        return 1;
+      }
+      return 0;
+    });
+
+    for (let i = 0; i < 5; i++) {
+      list.push(
+        <li>
+          <span>{i + 1}</span>
+          {array[i].name} - {array[i].cropCapacity}
+        </li>
+      );
+    }
+    return list;
+  }
+  function displayTopFiveCollectedArea() {
+    let list = [];
+
+    let array = agronomicStatistics.sort(function (a, b) {
+      if (a.collectedArea > b.collectedArea) {
+        return -1;
+      }
+      if (a.collectedArea < b.collectedArea) {
+        return 1;
+      }
+      return 0;
+    });
+    for (let i = 0; i < 5; i++) {
+      list.push(
+        <li>
+          <span>{i + 1}</span>
+          {array[i].name} - {array[i].collectedArea}
+        </li>
+      );
+    }
+    return list;
+  }
+
+  function displayTopFiveProductionVolumes() {
+    let list = [];
+
+    let array = agronomicStatistics.sort(function (a, b) {
+      if (a.ProductionVolumes > b.ProductionVolumes) {
+        return -1;
+      }
+      if (a.ProductionVolumes < b.ProductionVolumes) {
+        return 1;
+      }
+      return 0;
+    });
+    for (let i = 0; i < 5; i++) {
+      list.push(
+        <li>
+          <span>{i + 1}</span>
+          {array[i].name} - {array[i].ProductionVolumes}
+        </li>
+      );
+    }
+    return list;
+  }
+
   const [scroll, setscroll] = useState(0);
 
   useEffect(() => {
@@ -21,7 +90,7 @@ export default function Statistik(props) {
   props.show ? (show.current = "show") : (show.current = " "); /* check class */
 
   return (
-    <section className={"statistics " + show.current}>   {/* adding class show */}
+    <section className={"statistics " + show.current}>  {/* adding class show */}
       <div className="wrapper">
         <div className="title">Наша статистика показує</div>
         <div className="statistic_content">
@@ -37,21 +106,7 @@ export default function Statistik(props) {
             <div className="statistic_card">
               <div className="stat_card_label">Зібрана площа (тис. га.)</div>
               <ul className="stat_card_top_list">
-                <li>
-                  <span>1</span>Дніпропетровська область - 599,7
-                </li>
-                <li>
-                  <span>2</span>Кіровоградська область - 594,6
-                </li>
-                <li>
-                  <span>3</span>Харківська область - 573,3
-                </li>
-                <li>
-                  <span>4</span>Запорізька область - 532,5
-                </li>
-                <li>
-                  <span>5</span>Миколаївська область - 495,1
-                </li>
+                {displayTopFiveCollectedArea()}
               </ul>
               <div className="stat_card_at_ukraine">
                 По Україні - <span>6 523,9 тис. га.</span>
@@ -60,21 +115,7 @@ export default function Statistik(props) {
             <div className="statistic_card">
               <div className="stat_card_label">Обсяги виробництва (тис.ц.)</div>
               <ul className="stat_card_top_list">
-                <li>
-                  <span>1</span>Кіровоградська область - 15903,9
-                </li>
-                <li>
-                  <span>2</span>Харківська область - 14 300,9
-                </li>
-                <li>
-                  <span>3</span>Дніпропетровська область - 14 032,6
-                </li>
-                <li>
-                  <span>4</span>Миколаївська область - 11 237,5
-                </li>
-                <li>
-                  <span>5</span>Запорізька область - 10 583,8
-                </li>
+                {displayTopFiveProductionVolumes()}
               </ul>
               <div className="stat_card_at_ukraine">
                 По Україні - <span>164 398,4 тис. ц.</span>
@@ -85,21 +126,7 @@ export default function Statistik(props) {
                 Урожайність (ц. за 1 га. зібраної площі)
               </div>
               <ul className="stat_card_top_list">
-                <li>
-                  <span>1</span>Тернопільська область - 34,3
-                </li>
-                <li>
-                  <span>2</span>Вінницька область - 33,1
-                </li>
-                <li>
-                  <span>3</span>Черкаська область - 32,5
-                </li>
-                <li>
-                  <span>4</span>Хмельницька область - 32,1
-                </li>
-                <li>
-                  <span>5</span>Київська область - 31,2
-                </li>
+                {displayTopFiveCropCapacity()}
               </ul>
               <div className="stat_card_at_ukraine">
                 По Україні - <span>25,2 ц. за 1 га. зібраної площі</span>
